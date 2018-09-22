158bc766fd0c0af0ca73b2b8ddab07126ceafdfb
alkresin <alkresin@yahoo.com>
2018-09-22 11:10:52 +0300

    sample forms added, tests renamed

---
+++ tests/test_form1.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	egui "github.com/alkresin/external"
+)
+
+func main() {
+
+	if !egui.Init("") {
+		return
+	}
+
+	f := func (p []string)string {
+		egui.EvalProc("oLabel1:SetText(\"Hi, friends! (" + p[0] + ")\")")
+		return ""
+	}
+	egui.RegFunc("fmenu1", f)
+
+	f = func (p []string)string {
+		if p == nil {
+		}
+		return "Hi from Go!"
+	}
+	egui.RegFunc("fmenu2", f)
+
+	egui.OpenMainForm("../forms/example.xml")
+
+	egui.Exit()
+
+}
+
