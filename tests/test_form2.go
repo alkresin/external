34891738bf8fc3714de0008931359c52eaa745bb
alkresin <alkresin@yahoo.com>
2018-09-22 11:41:36 +0300

    test3.go renamed to test_form2.go

---
+++ tests/test_form2.go
@@ -0,0 +1,36 @@
+package main
+
+import egui "github.com/alkresin/external"
+
+func main() {
+
+	if !egui.Init("") {
+		return
+	}
+
+	pWindow := &(egui.Widget{X: 100, Y: 100, W: 400, H: 280, Title: "External"})
+	egui.InitMainWindow(pWindow)
+
+	egui.Menu("")
+	egui.Menu( "File" )
+	egui.AddMenuItem( "Open form", openf, "openf" )
+	egui.AddMenuSeparator()
+	egui.AddMenuItem( "Exit", nil, "hwg_EndWindow()" )
+	egui.EndMenu()
+	egui.Menu( "Help" )
+	egui.AddMenuItem( "About", nil, "hwg_MsgInfo(hb_version()+chr(10)+chr(13)+hwg_version(),\"About\")" )
+	egui.EndMenu()
+	egui.EndMenu()
+
+	pWindow.Activate()
+
+	egui.Exit()
+}
+
+func openf(p []string)string {
+
+	if p == nil {
+	}
+	egui.OpenForm("../forms/testget2.xml")
+	return ""
+}
